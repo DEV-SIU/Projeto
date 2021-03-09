ee215e8f18317e9fc03cc4d5a64738921fd137f4
DEV-SIU <dtmartins102@gmail.com>
2021-03-09 16:41:14 -0300

    Atualização: Modificação da função cpf  para private set

--- src/main/kotlin/one/digitalnnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalnnovation/digionebank/Pessoa.kt
@@ -1,12 +1,16 @@
 package one.digitalnnovation.digionebank
 
 class Pessoa {
-    val nome: String = "Lucas"
-    val cpf:String = "123.456.789.10"
+    var nome: String = "Lucas"
+    var cpf:String = "123.456.789.10"
+    //Private set vai privar o setar das variaveis externos
+    private set
+
 }
 
 fun main() {
     val lucas = Pessoa()
+    println(lucas)
     println(lucas.nome)
     println(lucas.cpf)
 }
