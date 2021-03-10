1376770637b49cba9eb22c427306e7263743c9f4
DEV-SIU <dtmartins102@gmail.com>
2021-03-10 17:28:56 -0300

    Atualização: Notas de Aula

--- src/main/kotlin/one/digitalnnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalnnovation/digionebank/Pessoa.kt
@@ -2,15 +2,32 @@ package one.digitalnnovation.digionebank
 
 class Pessoa {
     var nome: String = "Lucas"
+
+    //private vai privar as variavel (var ou val) de ser setada ou dar get fora da class pertencente
+    /*private var cpf:String = "123.456.789.10"*/
+
     var cpf:String = "123.456.789.10"
-    //Private set vai privar o setar das variaveis externos
+
+    //Private set vai privar o setar das variavel fora da class,somente poderar setar dentro da propria class
     private set
+
+    //inner class usado para criar um class interna dentro de uma  class
+   /* inner class Endereco{
+        var rua: String = "Rua das caixibinhas"
+    }*/
 
 }
 
 fun main() {
     val lucas = Pessoa()
-    println(lucas)
+
+    //println(lucas)
+
+
     println(lucas.nome)
     println(lucas.cpf)
+
+
+    //chamando class interna
+    //println(lucas.Endereco().rua)
 }
